847870f916a593fc3b7e5615957e856132f5254b
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-23 11:09:28 +0100

    Binding OpenAI Responses With JSON Schema

--- src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -2,12 +2,13 @@ package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 
 public interface OpenAIService {
 
     Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest);
-    Answer getCapital(GetCapitalRequest getCapitalRequest);
+    GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest);
     String getAnswer(String question);
 
     Answer getAnswer(Question question);
