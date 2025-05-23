e731dd75d69affe6e3ecafdace430655d8c3a272
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-23 10:03:46 +0100

    Using Prompt Templates

--- src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIService.java
@@ -1,10 +1,12 @@
 package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
+import guru.springframework.springaiintro.model.GetCapitalRequest;
 import guru.springframework.springaiintro.model.Question;
 
 public interface OpenAIService {
 
+    Answer getCapital(GetCapitalRequest getCapitalRequest);
     String getAnswer(String question);
 
     Answer getAnswer(Question question);
